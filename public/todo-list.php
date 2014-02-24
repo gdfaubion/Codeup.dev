1b62841f35d29df9f52b97272e515c708fa28628
gdfaubion <gdfaubion@gmail.com>
2014-02-24 16:33:44 -0600

    HTML w/ PHP

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,28 +1,71 @@
 <?php
-echo "<p>POST:</p>";
+var_dump($_GET);
 var_dump($_POST);
 
-echo "<p>GET:</p>";
-var_dump($_GET);
+$file = "data/tododata.txt";
+
+function read_file() {
+	$file = "data/tododata.txt";
+    $handle = fopen($file, "r");
+    $setfile = filesize($file);
+    if($setfile > 0) {
+    $contents = fread($handle, filesize($file));
+    fclose($handle);
+    return explode("\n", $contents);
+   } else {
+   	echo "Your dont have any items in your list";
+   	return array();
+   }  
+}
+
+$items = read_file($file);
+
+
+function saveFile($file, $item) {
+	$itemstr = implode("\n", $item);
+	$handle = fopen($file, 'w');
+	fwrite($handle, $itemstr);
+	fclose($handle);
+}
+
+if(isset($_POST["item"])) {
+	$newitem = $_POST["item"];
+	array_push($items, $newitem);
+	saveFile($file, $items);
+
+}
+
+
+
+if(isset($_GET["remove"])) {
+	$key = $_GET["remove"];
+	unset($items[$key]);
+	saveFile($file, $items);
+
+	header("Location: todo-list.php");
+	exit;
+
+}
+
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Todo List</title>
+		<title>TODO List</title>
 	</head>
 	<body>
-		<h1>TODO List:</h1>
+		<h1>TO-DO List:</h1>
 		<ul>
-			<li>Wash the car</li>
-			<li>Feed the dog</li>
-			<li>Take out the trash</li>
+			<?php foreach($items as $key => $item) { ?>
+			<li> <?php echo $item; ?> <a href="?remove=<?php echo $key; ?>"> Completed </a> </li>
+				<?php } ?>
 		</ul>
 		<hr>
-		<h2>Add items to the todo list</h2>
-		<form method="GET" action="">
+		<h2>Add Items to the TO-DO List</h2>
+		<form method="POST" action="todo-list.php">
 			<p>
-				<label for="Item">Item:</label>
+				<label for="item"><strong>Item:</strong></label>
 				<input type="text" id="item" name="item" placeholder="Enter New Item">
 			</p>
 			<p>
