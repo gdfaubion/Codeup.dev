2c157877353e2f258825ad0344b2954b1a5aa598
gdfaubion <gdfaubion@gmail.com>
2014-02-25 20:57:44 -0600

    HTML w/ PHP refactored
    added overwrite checkbox, archive feature and striptags/specialchar.

--- public/todo-list.php
+++ public/todo-list.php
@@ -1,102 +1,109 @@
 <?php
-// var_dump($_GET);
-// var_dump($_POST);
-
-$file = "data/tododata.txt";
-
+// read from txt file, return array
 function read_file($file) {
     $handle = fopen($file, "r");
-    $setfile = filesize($file);
-    if($setfile > 0) {
-    $contents = fread($handle, filesize($file));
+   	$contents = fread($handle, filesize($file));
     fclose($handle);
-    return explode("\n", $contents);
-   } else {
-   	echo "You don't have anything To-Do! How about a nap?";
-   	return array();
-   }  
+    return explode("\n", $contents); 	
 }
 
-$items = read_file($file);
-
-
-function saveFile($file, $item) {
-	$itemstr = implode("\n", $item);
-	$handle = fopen($file, 'w');
-	fwrite($handle, $itemstr);
-	fclose($handle);
+// save to array txt file
+function save_file($file, $item) {
+    $handle = fopen($file, 'w');
+    $itemstr = implode("\n", $item);
+    fwrite($handle, $itemstr);
+    fclose($handle);
 }
 
-if (!empty($_POST["item"])) {
-	$newitem = $_POST["item"];
-	array_push($items, $newitem);
-	saveFile($file, $items);
+// set file location
+$file = "data/tododata.txt";
+$archiveFile = "data/archives.txt";
+$archives = read_file($archiveFile);
+// check that file is not empty
+$items = (filesize($file) > 0) ? read_file($file) : array();
 
-}
-
-if (isset($_GET["remove"])) {
-	$key = $_GET["remove"];
-	unset($items[$key]);
-	saveFile($file, $items);
-
+// add items to list
+if (!empty($_POST['newItem'])) {
+	array_push($items, $_POST['newItem']);
+	save_file($file, $items);
 	header("Location: todo-list.php");
 	exit;
+}
 
+// remove items from list
+if (isset($_GET['remove'])) {
+	$archiveItem = array_splice($items, $_GET['remove'], 1);
+	$archives = array_merge($archives, $archiveItem);
+	save_file($archiveFile, $archives);
+	save_file($file, $items);
+	header("Location: todo-list.php");
+	exit;
+}
+
+$errorMessage = '';
+// upload file, if not empty and is text file
+if (count($_FILES) > 0) {
+	if($_FILES['file1']['error'] != 0) {
+		$errorMessage = 'Error Uploading File!!';
+	} elseif ($_FILES['file1']['type'] != 'text/plain') {
+		$errorMessage = 'ERROR!! Invalid File!';
+	} else {
+		$upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+		$filename = basename($_FILES['file1']['name']);
+		$saved_filename = $upload_dir . $filename;
+		move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+		$fileContents = read_file($saved_filename);
+	
+		if ($_POST['file2'] == TRUE) {
+			$items = $fileContents;
+		} else {
+			$items = array_merge($items, $fileContents);
+		}
+
+		save_file($file, $items);		
+		header("Location: todo-list.php");
+		exit;
+	}
 }
 
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>TODO List</title>
-	</head>
-	<body>
-		<h1>TO-DO List:</h1>
+<head>
+	<title>TODO List</title>
+</head>
+<body>
+
+	<h2>TO-DO List</h2>
 		<ul>
-			<?php foreach($items as $key => $item) { ?>
-			<li> <?php echo $item; ?> <a href="?remove=<?php echo $key; ?>"> To-Done! </a> </li>
-				<?php } ?>
+			<? foreach ($items as $key => $item): ?>
+				<li><?= htmlspecialchars(strip_tags($item)); ?> <a href="?remove=<?= $key; ?>">To-Done! </a></li>
+			<? endforeach; ?>
 		</ul>
 		<hr>
-		<h2>Add Items to the TO-DO List</h2>
-		<form method="POST" action="todo-list.php">
+		<h2>Add Items to the To-Do List:</h2>
+		<form method="POST" enctype="multipart/form-data" action="todo-list.php">
 			<p>
-				<label for="item"><strong>Item:</strong></label>
-				<input type="text" id="item" name="item" placeholder="Enter New Item" autofocus>
+				<label for="newItem"><strong>New Item:</strong></label>
+				<input id="newItem" name="newItem" type="text" autofocus="autofocus">
 			</p>
 			<p>
-				<button type="Confirm">Confirm</button>
+				<? if (!empty($errorMessage)) : ?>
+				<?= $errorMessage; ?>
+				<? endif; ?>
 			</p>
+			<p>
+        		<label for="file1"><strong>Upload a file to add to your list:</strong></label>
+        		<input id="file1" name="file1" type="file">
+    		</p>
+    		<p>
+        		<label for="file2">Replace To-Do list items with contents of this file? </label>
+        		<input id="file2" name="file2" type="checkbox">
+    		</p>
+
+			<button type="submit">Submit</button>
 		</form>
-	</body>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</body>
 </html>
