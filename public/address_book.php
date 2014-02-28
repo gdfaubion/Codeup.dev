3ace5913fd1031a0d7207cb8b3d8dd42681f6509
gdfaubion <gdfaubion@gmail.com>
2014-02-28 10:09:59 -0600

    HTML w/ PHP & CSV Files
    moved addressDataStore class to a separate file and used required_once
    to include it.

--- public/address_book.php
+++ public/address_book.php
@@ -1,44 +1,6 @@
 <?php
-class addressDataStore {
 
-	public $filename = '';
-
-	function __construct($filename = 'data/address_book.csv') 
-	{
-		$this->filename = $filename;
-
-	}
-
-	function readCSV()
-	{
-		$address_book = [];
-		$handle = fopen($this->filename, "r");
-		while(!feof($handle)) {
-		$line = fgetcsv($handle);
-		if(!empty($line)) 
-		{
-	  		$address_book[] = $line;
-			}
-		}
-		fclose($handle);
-		return $address_book;
-
-	}
-
-	function writeCSV($entries)
-	{
-		$handle = fopen($this->filename, "w");
-
-		foreach ($entries as $entry) 
-		{
-			fputcsv($handle, $entry);
-		}
-
-		fclose($handle);
-
-	}
-
-}
+require_once('classes/address_data_store.php');
 
 $contacts = new addressDataStore();
 
