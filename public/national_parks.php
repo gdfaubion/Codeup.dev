62ed5aaf4d0f8676b5561011c5706d61c2f8708b
gdfaubion <gdfaubion@gmail.com>
2014-03-19 16:03:24 -0500

    Jquery
    added event listeners in Jquery

--- public/national_parks.php
+++ public/national_parks.php
@@ -38,12 +38,16 @@
 	<link rel="stylesheet" href="/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/css/portfolio.css">
 	<link href='http://fonts.googleapis.com/css?family=Belgrano' rel='stylesheet' type='text/css'>
+	<script src="/js/jquery.js"></script>
 	<style type="text/css">
 		body{
 			font-family: 'Belgrano', serif;
 		}
 		#error{
 			color: red;
+		}
+		.invisible{
+			display: none;
 		}
 	</style>	
 </head>
@@ -156,7 +160,51 @@
 			<br>
 			<p>*All information used was taken from <a href="http://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States">Wikipedia</a>.</p>
 		</div>
+	</div>
+	<button class="btn btn-success toggle">Display Answers</button>
+	<br>
+	<br>
+	<div class="col-md-12">
+		<dl>
+			<dt>What is the National Parks Conservation Association?</dt>
+				<dd>We are an independent, nonpartisan voice working to address major threats facing the National Park System. NPCA was established in 1919, just three years after the National Park Service. Stephen Mather, the first director of the Park Service, was one of our founders. He felt very strongly that the national parks would need an independent voice—outside the political system—to ensure these places remained unimpaired for future generations. We have more than 800,000 members and supporters. In addition to our national headquarters in Washington, D.C., NPCA has 24 regional and field offices around the country. Our mission: to protect and enhance America’s National Park System for present and future generations.</dd>
+				<br>
+			<dt>What does NPCA do?</dt>
+				<dd>NPCA works to address major threats facing the National Park System by gathering the information we need through our two centers, Center for Park Management and Center for Park Research, by keeping our eyes and ears to the ground in 11 regional and 12 field offices, and by developing relationships on Capitol Hill and in the administration to counter legislation or policies that would adversely affect the parks. We also pursue solutions in the courts when other avenues fail us.</dd>
+				<br>
+			<dt>How do I contact NPCA?</dt>
+				<dd>Please feel free to email us at npca@npca.org, call us at 800.628.7275, or write to us at 777 6th St. NW, Suite 700, Washington, DC 20001.</dd>
+				<br>
+			<dt>How can I become a member? </dt>
+				<dd>You can join NPCA online or by calling 800.628.7275 and asking for our member services department.</dd>
+				<br>
+			<dt>What are membership benefits? </dt>
+				<dd>Our membership benefits include an award-winning quarterly magazine, National Parks; our exclusive travel information kit, which includes maps, a road atlas, park guides, and more; and discounts on NPCA publications and merchandise.</dd>
+				<br>
+			<dt>How do I renew or let you know about a change of address?</dt>
+				<dd>We will send you renewal notices before your membership expires. You can return the renewal form with a check or credit card number, renew online, or call us at 800.628.7275.</dd>
+				<br>
+			<dt>How do I subscribe to National Parks magazine?</dt>
+				<dd>For just $15, you can subscribe online or by calling our member services department at 800-628-7275.</dd>
+				<br>
+			<dt>What are national parks?</dt>
+				<dd>The U.S. National Park System contains 401 units, which highlight and protect our natural, historic, and cultural treasures. The world's first national park, Yellowstone, was established in 1872. The National Park Service, the agency that oversees the park system, was created in 1916. Today, national parks cover more than 83 million acres and can be found in every state except Delaware, as well as the District of Columbia, American Samoa, Guam, Puerto Rico, and the Virgin Islands. The largest park is Wrangell-St. Elias National Park and Preserve which covers 13.2 million acres in Alaska. At .02 acres, Thaddeus Kosciuszko National Memorial in Pennsylvania is the smallest.</dd>
+				<br>
+			<dt>What problems do the national parks face?</dt>
+				<dd>Although the National Park Service strives to safeguard our national parks, many of the 401 sites suffer from a variety of problems. Insufficient funding is the single greatest threat facing the parks. Some of the nation’s parks have some of the worst air in the country, even though our laws say they should have the cleanest. And, parks are such attractive places to live and work that commercial and other development threatens to overwhelm some of our most cherished lands with subdivisions and commercial development.</dd>
+				<br>
+			<dt>Can I purchase a National Parks Pass from NPCA?</dt>
+				<dd>Due to changes in the National Parks Pass program, passes must now be purchased from participating federal agencies. Click here to purchase your parks pass from USGS.</dd>
+				<br>
+		</dl>
 	</div> 
+	<script type="text/javascript">
+		$('dd').addClass('invisible');
+		$('.toggle').on('click', function() {
+			$('dd').toggleClass('invisible');
+		})
+
+	</script>
 </body>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="/js/bootstrap.min.js"></script>
